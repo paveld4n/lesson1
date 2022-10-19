1bfb0c1ed10f539c0b1846535368e1022898fe23
Pavel <rut100500@yandex.ru>
2022-10-19 07:56:51 +0300

    Привет Маша

---
+++ Example0005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Как тебя зовут дружище? Напиши: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Машкаааа, привеет!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
